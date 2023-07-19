02f7c700c91bc662f3d6aee0e087ef339924f14a
sohil <sohils966@gmail.com>
2023-07-19 13:18:13 -0400

    Final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "fhueujcfzugumgfoabgj.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
